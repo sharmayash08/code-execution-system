051be23fa4616409fed07b914b106583736d6f0d
Yash Sharma <yash280876@gmail.com>
2025-02-05 01:36:09 +0530

    Fixed a Bug

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { execSync, exec } = require("child_process");
 
 const app = express();
 app.use(cors({
-    origin: 'http//localhost:5173', 
+    origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
